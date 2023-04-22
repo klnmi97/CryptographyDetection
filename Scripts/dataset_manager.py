789410c7042b7f1ed12ee9129a427ec6d11e7a13
Mickey Klunko <michael.klunko@gmail.com>
2023-04-22 16:25:12 -0400

    Dataset manager: Add sample lists generations

--- Scripts/dataset_manager.py
+++ Scripts/dataset_manager.py
@@ -11,6 +11,17 @@ import zlib
 import pefile
 
 
+def write_chunks_to_files(lst, n):
+    # Divide list into chunks of size n
+    chunks = [lst[i:i+n] for i in range(0, len(lst), n)]
+    
+    # Write each chunk to a separate file
+    for i, chunk in enumerate(chunks):
+        with open(f"samples_{i}.list", "w") as f:
+            #f.write('\n'.join(chunk))
+            for item in chunk:
+                f.write("%s\n" % item)
+
 def get_samples_from_file(path):
     with open(path, 'r') as file:
         lines = file.readlines()
@@ -33,6 +44,8 @@ def create_sample_set(dp_path, n='all', category='is_malware'):
 
     if n != 'all':
         samples_len = len(sample_sha256)
+        if n >= samples_len:
+             raise Exception("Cannot generate a sample list larger than samples available")
 
         i = 0
         while i < n:
@@ -112,10 +125,13 @@ if __name__ == '__main__':
     samples = 'all'
     metadb_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'meta.db')
     decompressed_path=''
+    gen_quantity = 0
+    batches = 1
 
     parser = argparse.ArgumentParser(description='Download and modify malware samples.')
-    parser.add_argument('path', type=str, help='path where to download the malware samples from S3')
+    parser.add_argument('path', type=str, help='path where to download the malware samples from S3 or save the sample list.')
     parser.add_argument('-n', '--num_samples', type=int, help='Number of samples to download randomly')
+    parser.add_argument('-g', '--gen_list', nargs=2, type=int, metavar=('quantity', 'size'), help='Generate a random list of samples and save into files using the provided path. Can be used together with the category argument.')
     parser.add_argument('-c', '--category', type=str, help='Category of malware to download. See categories at https://ai.sophos.com/2020/12/14/sophos-reversinglabs-sorel-20-million-sample-malware-dataset/')
     parser.add_argument('-d', '--decompress', type=str, help='Path where to store decompressed samples')
     parser.add_argument('-r', '--restore', action='store_true', help='restore header bytes')
@@ -136,11 +152,23 @@ if __name__ == '__main__':
     if args.num_samples:
         samples = args.num_samples
 
+    
+    if args.gen_list:
+        samples, batches = args.gen_list
+
     if args.decompress:
         decompressed_path = args.decompress
 
-    samples_list = create_sample_set(metadb_path, args.num_samples, category)
+    samples_list = create_sample_set(metadb_path, samples, category)
     
+    # If option -g is active, create we write the generated list and exit
+    if args.gen_list:
+        # save_path = os.path.join(args.path, "samples.list")
+        # with open(save_path, 'w') as f:
+        #     for item in samples_list:
+        #         f.write("%s\n" % item)
+        write_chunks_to_files(samples_list, batches)
+        sys.exit(0)
 
     download_samples(args.path, samples_list)
 
